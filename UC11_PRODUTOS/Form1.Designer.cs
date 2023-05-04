8d8a8c05a3ed86d890e0b4cb74c507bcdd233e65
lucasggalvao <lucas11galvao@gmail.com>
2023-05-03 21:50:31 -0300

    Adicionar arquivos de projeto.

---
+++ UC11_PRODUTOS/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace UC11_PRODUTOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelProduto = new System.Windows.Forms.Label();
+            this.labelPreco = new System.Windows.Forms.Label();
+            this.labelCategoria = new System.Windows.Forms.Label();
+            this.textBoxDescricao = new System.Windows.Forms.TextBox();
+            this.textBoxCategoria = new System.Windows.Forms.TextBox();
+            this.textBoxPreco = new System.Windows.Forms.TextBox();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.buttonLIMPAR = new System.Windows.Forms.Button();
+            this.buttonExcluir = new System.Windows.Forms.Button();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewProdutos = new System.Windows.Forms.DataGridView();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelProduto
+            // 
+            this.labelProduto.AutoSize = true;
+            this.labelProduto.BackColor = System.Drawing.Color.Transparent;
+            this.labelProduto.ForeColor = System.Drawing.Color.White;
+            this.labelProduto.Location = new System.Drawing.Point(12, 92);
+            this.labelProduto.Name = "labelProduto";
+            this.labelProduto.Size = new System.Drawing.Size(110, 13);
+            this.labelProduto.TabIndex = 0;
+            this.labelProduto.Text = "Descrição do Produto";
+            this.labelProduto.Click += new System.EventHandler(this.labelProduto_Click);
+            // 
+            // labelPreco
+            // 
+            this.labelPreco.AutoSize = true;
+            this.labelPreco.BackColor = System.Drawing.Color.Transparent;
+            this.labelPreco.ForeColor = System.Drawing.Color.White;
+            this.labelPreco.Location = new System.Drawing.Point(12, 306);
+            this.labelPreco.Name = "labelPreco";
+            this.labelPreco.Size = new System.Drawing.Size(90, 13);
+            this.labelPreco.TabIndex = 1;
+            this.labelPreco.Text = "Preço do Produto";
+            this.labelPreco.Click += new System.EventHandler(this.labelPreco_Click);
+            // 
+            // labelCategoria
+            // 
+            this.labelCategoria.AutoSize = true;
+            this.labelCategoria.BackColor = System.Drawing.Color.Transparent;
+            this.labelCategoria.ForeColor = System.Drawing.Color.White;
+            this.labelCategoria.Location = new System.Drawing.Point(12, 202);
+            this.labelCategoria.Name = "labelCategoria";
+            this.labelCategoria.Size = new System.Drawing.Size(110, 13);
+            this.labelCategoria.TabIndex = 2;
+            this.labelCategoria.Text = "Catergoria do Produto";
+            this.labelCategoria.Click += new System.EventHandler(this.labelCategoria_Click);
+            // 
+            // textBoxDescricao
+            // 
+            this.textBoxDescricao.Location = new System.Drawing.Point(15, 108);
+            this.textBoxDescricao.Name = "textBoxDescricao";
+            this.textBoxDescricao.Size = new System.Drawing.Size(259, 20);
+            this.textBoxDescricao.TabIndex = 3;
+            this.textBoxDescricao.TextChanged += new System.EventHandler(this.textBoxProduto_TextChanged);
+            // 
+            // textBoxCategoria
+            // 
+            this.textBoxCategoria.Location = new System.Drawing.Point(15, 218);
+            this.textBoxCategoria.Name = "textBoxCategoria";
+            this.textBoxCategoria.Size = new System.Drawing.Size(259, 20);
+            this.textBoxCategoria.TabIndex = 4;
+            this.textBoxCategoria.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBoxPreco
+            // 
+            this.textBoxPreco.Location = new System.Drawing.Point(15, 322);
+            this.textBoxPreco.Name = "textBoxPreco";
+            this.textBoxPreco.Size = new System.Drawing.Size(259, 20);
+            this.textBoxPreco.TabIndex = 5;
+            this.textBoxPreco.TextChanged += new System.EventHandler(this.textBoxPreco_TextChanged);
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(15, 363);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(116, 23);
+            this.buttonCadastrar.TabIndex = 6;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // buttonLIMPAR
+            // 
+            this.buttonLIMPAR.Location = new System.Drawing.Point(137, 363);
+            this.buttonLIMPAR.Name = "buttonLIMPAR";
+            this.buttonLIMPAR.Size = new System.Drawing.Size(137, 23);
+            this.buttonLIMPAR.TabIndex = 7;
+            this.buttonLIMPAR.Text = "Limpar";
+            this.buttonLIMPAR.UseVisualStyleBackColor = true;
+            this.buttonLIMPAR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonExcluir
+            // 
+            this.buttonExcluir.Location = new System.Drawing.Point(15, 469);
+            this.buttonExcluir.Name = "buttonExcluir";
+            this.buttonExcluir.Size = new System.Drawing.Size(116, 23);
+            this.buttonExcluir.TabIndex = 8;
+            this.buttonExcluir.Text = "Excluir";
+            this.buttonExcluir.UseVisualStyleBackColor = true;
+            this.buttonExcluir.Click += new System.EventHandler(this.buttonExcluir_Click);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(137, 469);
+            this.textBoxID.Multiline = true;
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(137, 23);
+            this.textBoxID.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Digite abaixo o ID que deseja excluir";
+            // 
+            // dataGridViewProdutos
+            // 
+            this.dataGridViewProdutos.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridViewProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProdutos.Location = new System.Drawing.Point(306, 92);
+            this.dataGridViewProdutos.Name = "dataGridViewProdutos";
+            this.dataGridViewProdutos.Size = new System.Drawing.Size(303, 400);
+            this.dataGridViewProdutos.TabIndex = 11;
+            this.dataGridViewProdutos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProdutos_CellContentClick);
+            // 
+            // buttonAtualizar
+            // 
+            this.buttonAtualizar.Location = new System.Drawing.Point(306, 495);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(303, 23);
+            this.buttonAtualizar.TabIndex = 12;
+            this.buttonAtualizar.Text = "Atualizar";
+            this.buttonAtualizar.UseVisualStyleBackColor = true;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UC11_PRODUTOS.Properties.Resources.embiid;
+            this.ClientSize = new System.Drawing.Size(621, 609);
+            this.Controls.Add(this.buttonAtualizar);
+            this.Controls.Add(this.dataGridViewProdutos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.buttonExcluir);
+            this.Controls.Add(this.buttonLIMPAR);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.textBoxPreco);
+            this.Controls.Add(this.textBoxCategoria);
+            this.Controls.Add(this.textBoxDescricao);
+            this.Controls.Add(this.labelCategoria);
+            this.Controls.Add(this.labelPreco);
+            this.Controls.Add(this.labelProduto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProduto;
+        private System.Windows.Forms.Label labelPreco;
+        private System.Windows.Forms.Label labelCategoria;
+        private System.Windows.Forms.TextBox textBoxDescricao;
+        private System.Windows.Forms.TextBox textBoxCategoria;
+        private System.Windows.Forms.TextBox textBoxPreco;
+        private System.Windows.Forms.Button buttonCadastrar;
+        private System.Windows.Forms.Button buttonLIMPAR;
+        private System.Windows.Forms.Button buttonExcluir;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewProdutos;
+        private System.Windows.Forms.Button buttonAtualizar;
+    }
+}
+
---
+++ UC11_PRODUTOS/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC11_PRODUTOS
+{
+    public partial class Form1 : Form
+    {
+
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        public Form1()
+        {
+            InitializeComponent();
+            servidor = "Server=localhost;Database=bd_produtos;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            bool duplicado = false;
+
+            try
+            {
+                conexao.Open();
+                comando.CommandText = " SELECT descricao FROM tbl_produtos WHERE descricao = '" + textBoxDescricao.Text + "'";
+                MySqlDataReader resultado = comando.ExecuteReader();
+
+                if (resultado.Read())
+                {
+                   duplicado = true;
+                   MessageBox.Show("Registro Duplicado! Verifique os valores digitados");
+                }          
+            }
+            catch (Exception erro_mysql)
+            {
+                
+            }
+            finally
+            {
+                conexao.Close();   
+            }
+
+            if (duplicado == false)
+            {
+                try
+                {
+                    conexao.Open();
+                    comando.CommandText = "INSERT INTO tbl_produtos(Descricao, Categoria, Preco) VALUES ('" + textBoxDescricao.Text + "', '" + textBoxCategoria.Text + "'," + textBoxPreco.Text.Replace(",", ".") + ");";
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Produto cadastrado com sucesso!");
+                }
+                catch (Exception erro_mysql)
+                {
+                    MessageBox.Show(erro_mysql.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+            }
+        }
+
+        private void textBoxPreco_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCategoria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPreco_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelProduto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxPreco.Text = "";
+            textBoxCategoria.Text = "";
+            textBoxDescricao.Text = "";
+        }
+
+        private void buttonExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                conexao.Open();
+                comando.CommandText = "DELETE FROM tbl_produtos WHERE id = '" + textBoxID.Text + "';";
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Produto excluido com sucesso!");
+
+
+            }
+            catch (Exception erro)
+            {
+                //MessageBox.Show(erro.Message);
+                MessageBox.Show("Erro ao excluir produto. Tente Novamente");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "SELECT * FROM tbl_produtos;";
+
+                MySqlDataAdapter adaptadorProdutos = new MySqlDataAdapter(comando);
+
+                DataTable tabelaProdutos  = new DataTable();
+                adaptadorProdutos.Fill(tabelaProdutos);
+               
+                dataGridViewProdutos.DataSource = tabelaProdutos;
+                dataGridViewProdutos.Columns["id"].HeaderText = "Codigo";
+                dataGridViewProdutos.Columns["descricao"].HeaderText = "Descrição";
+                dataGridViewProdutos.Columns["categoria"].HeaderText = "Categoria";
+                dataGridViewProdutos.Columns["preco"].HeaderText = "Preço";
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void dataGridViewProdutos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxID.Text = dataGridViewProdutos.CurrentRow.Cells[0].Value.ToString();
+        }
+    }
+}
